e2493aa2e29edabe1a47f279e245b3fd753e9529
PeachParfa1t <perfilev621@gmail.com>
2025-05-11 12:23:01 +0800

    тест

--- ParticleSystem/Form1.Designer.cs
+++ ParticleSystem/Form1.Designer.cs
@@ -59,9 +59,9 @@
             // 
             // picDisplay
             // 
-            picDisplay.Location = new Point(-10, -4);
+            picDisplay.Location = new Point(12, -4);
             picDisplay.Name = "picDisplay";
-            picDisplay.Size = new Size(1179, 435);
+            picDisplay.Size = new Size(1157, 435);
             picDisplay.TabIndex = 0;
             picDisplay.TabStop = false;
             picDisplay.MouseMove += picDisplay_MouseMove;
